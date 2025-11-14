d672f31db5374f8e44cc8175efa69a273a0ff78e
pywoo <kfdsy0103@naver.com>
2025-11-15 00:50:19 +0900

    fix: WebDriverManager 의존성 삭제
    WebDriverManager 의존성이 SpringAI 의존성과 Jackson 관련 충돌이 발생하여 삭제하였습니다.

--- src/main/java/jobkorea/crawler/config/WebDriverFactory.java
+++ src/main/java/jobkorea/crawler/config/WebDriverFactory.java
@@ -1,6 +1,5 @@
 package jobkorea.crawler.config;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,10 +10,7 @@ public class WebDriverFactory {
 
     public WebDriver createChromeDriver() {
 
-        // 1. WebDriverManager를 사용하여 ChromeDriver 자동 설정
-        WebDriverManager.chromedriver().setup();
-
-        // 2. 요청한 ChromeOptions 설정
+        // ChromeOptions 설정
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized"); // 브라우저 전체 화면 표시
         options.addArguments("--disable-gpu", "--window-size=1920,1080");
@@ -25,11 +21,11 @@ public class WebDriverFactory {
         options.addArguments("--remote-debugging-port=9222");  // 웹소켓 디버깅 활성화
         options.addArguments("--disable-popup-blocking"); // 팝업 미사용
         // 멀티스레딩 시 포트 충돌 처리 필요
-        
+
         // (선택사항) Headless 모드 (UI 없이 백그라운드에서 실행)
         // options.addArguments("--headless");
 
-        // 3. 옵션을 적용하여 ChromeDriver 인스턴스 생성 및 반환
+        // ChromeDriver 인스턴스 생성 및 반환
         return new ChromeDriver(options);
     }
 }
