cf7bb7aed0e8719fb61ab7e816c97f04bb5f2e67
pywoo <kfdsy0103@naver.com>
2025-11-20 14:15:11 +0900

    chore: 불필요 코드 삭제

--- src/main/java/jobkorea/crawler/controller/CrawlerController.java
+++ src/main/java/jobkorea/crawler/controller/CrawlerController.java
@@ -2,7 +2,6 @@ package jobkorea.crawler.controller;
 
 import java.util.List;
 import jobkorea.crawler.service.CrawlerService;
-import jobkorea.crawler.service.NaverOcrService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CrawlerController {
 
     private final CrawlerService crawlerService;
-    private final NaverOcrService naverOcrService;
 
     @GetMapping("/request-crawling")
     public void requestCrawling() {
