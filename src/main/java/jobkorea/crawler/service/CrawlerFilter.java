d47fa3cb0cf5689fbd62cc624ae44fa72ac488ce
pywoo <kfdsy0103@naver.com>
2025-11-24 12:23:45 +0900

    feat: 50개씩 공고를 보도록 필터 적용

--- src/main/java/jobkorea/crawler/service/CrawlerFilter.java
+++ src/main/java/jobkorea/crawler/service/CrawlerFilter.java
@@ -1,6 +1,6 @@
 package jobkorea.crawler.service;
 
-import java.util.List;
+import jobkorea.crawler.enums.JobCode;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,23 +10,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class CrawlerFilter {
 
-    public void selectFilter(WebDriverWait wait) throws InterruptedException {
+    public void selectWebDeveloperFilter(WebDriverWait wait) throws Exception {
 
         // "AI/개발/데이터" 필터 클릭
-        WebElement devDataFilter = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("label[for='duty_step1_10031']")));
+        WebElement devDataFilter = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector("label[for='duty_step1_10031']")));
         devDataFilter.click();
 
-        // 상세 분류에서 요소 체크 (우선 백엔드만)
-        List<String> jobValues = List.of("1000229");
+        String selector = "label[for='duty_step2_" + JobCode.웹개발.getJobCode() + "']";
 
-        for (String jobValue : jobValues) {
-            // Java에서는 문자열 포맷팅 대신 + 연산자로 CSS 선택자를 만듭니다.
-            String selector = "label[for='duty_step2_" + jobValue + "']";
+        WebElement jobCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(selector)));
+        wait.until(ExpectedConditions.elementToBeClickable(jobCheckbox)).click();
 
-            WebElement jobCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(selector)));
-            wait.until(ExpectedConditions.elementToBeClickable(jobCheckbox)).click();
-        }
+        searchAndSort(wait);
+    }
 
+    private void searchAndSort(WebDriverWait wait) throws InterruptedException {
         // "검색" 버튼 클릭
         WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("dev-btn-search")));
         searchButton.click();
@@ -37,5 +36,11 @@ public class CrawlerFilter {
                 By.cssSelector("#orderTab option[value='3']")
         ));
         optionToClick.click();
+
+        // 50개씩 보기
+        WebElement elementsPerPage = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("#pstab option[value='50']")
+        ));
+        elementsPerPage.click();
     }
 }
