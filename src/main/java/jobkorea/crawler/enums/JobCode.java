ae04d615e5afaaa28c3f8c5802ab7a3255d91bb4
pywoo <kfdsy0103@naver.com>
2025-11-24 12:34:31 +0900

    refact: JobCode Enum으로 리팩토링

---
+++ src/main/java/jobkorea/crawler/enums/JobCode.java
@@ -0,0 +1,12 @@
+package jobkorea.crawler.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum JobCode {
+    웹개발("1000231");
+
+    private final String jobCode;
+}
